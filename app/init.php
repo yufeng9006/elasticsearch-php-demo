d3e05110edc34c97ad6ca9108c3c55afb8bfc7cd
gitgaojun <1148743058@qq.com>
2017-03-16 22:54:03 +0800

    设置config host

--- app/init.php
+++ app/init.php
@@ -4,7 +4,8 @@ use Elasticsearch\ClientBuilder;
 
 require_once("vendor/autoload.php");
 
-$es = ClientBuilder::create()->build(['hosts' => '127.0.0.1:9200']);
+$es = ClientBuilder::create()->setHosts(['hosts' => '127.0.0.1:9200'])->build();
+//$es = ClientBuilder::create()->build();
 
 //require_once("vendor/autoload.php");
 //
